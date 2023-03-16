84c700c34adc0ad470e7078eee665d794ce1686f
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-03-16 08:46:01 +0100

    fix test error

--- src/__test__/home.test.js
+++ src/__test__/home.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from "react-router-dom";
 import renderer from 'react-test-renderer'
 import Home from '../component/home';
 import store from '../redux/store';
@@ -8,7 +9,9 @@ describe('Home', () => {
     it('renders correctly for reserved state', () => {
         const tree = renderer.create(
             <Provider store={store}>
+            <Router>
             <Home />
+            </Router>
             </Provider>
         );
         expect(tree).toMatchSnapshot();
