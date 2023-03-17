77cdb6ed6b8049bcce0270df0a585c992776a86b
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-03-17 02:03:13 +0100

    clean unused files from home component

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -11,86 +11,23 @@ const BASE_URL = 'https://api.artic.edu/api/v1/artworks?page=1'
 
 const Home = () => {
 
-    const Europe = [
-        "France",
-        "Russia",
-        "Germany",
-        "United Kingdom",
-        "Italy",
-        "Spain",
-        "Ukraine",
-        "Poland",
-        "Romania",
-        "Netherlands",
-        "Belgium",
-        "Czech Republic (Czechia)",
-        "Greece",
-        "Portugal",
-        "Sweden",
-        "Hungary",
-        "Belarus",
-        "Austria",
-        "Serbia",
-        "Switzerland",
-        "Bulgaria",
-        "Denmark",
-        "Finland",
-        "Slovakia",
-        "Norway",
-        "Ireland",
-        "Croatia",
-        "Moldova",
-        "Bosnia and Herzegovina",
-        "Albania",
-        "Lithuania",
-        "North Macedonia",
-        "Slovenia",
-        "Latvia",
-        "Estonia",
-        "Montenegro",
-        "Luxembourg",
-        "Malta",
-        "Iceland",
-        "Andorra",
-        "Monaco",
-        "Liechtenstein",
-        "San Marino",
-        "Holy See",
-    ]
-
     const { artwork, status, pagination } = useSelector((store) => store.art);
+    const [page, setPage] = useState(BASE_URL);
     const [nation, setNation] = useState('');
-    
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchData(page));
+    }, [dispatch, page]);
+
     const searchfilter = (e) => {
         const { value } = e.target;
         setNation(value);
       };
 
-    const meat = [];
-
-    Europe.forEach((nation) => { 
-        const me = artwork.filter((country) => country.place_of_origin === nation)
-        if (me.length) {
-            meat.push(me);
-        }
-    })
-
-
-    const [page, setPage] = useState(BASE_URL);
-
-    const navigate = useNavigate();
-
-    const nextPage = () => {
-        if (pagination.page_number < 9964) {
-            setTimeout(setPage(pagination.next_url), 1000)
-        } 
-    }
-
-    const prevPage = () => {
-        if (pagination.page_number > 1) {
-            setTimeout(setPage(pagination.prev_url), 1000)
-        }
-    }
+    const nextPage = () => setTimeout(setPage(pagination.next_url), 1000)
+    const prevPage = () => setTimeout(setPage(pagination.prev_url), 1000)
 
     const prev_page = (pagination.page_number > 1) ? (<i className="bi bi-arrow-left-circle-fill text-dark left_back" style={{ fontSize: 50 }}  onClick={prevPage}></i>) : '';
     const next_page = (pagination.page_number < 9964) ? (<i className="bi bi-arrow-right-circle-fill text-dark right_back" style={{ fontSize: 50 }}  onClick={nextPage}></i>) : '';
@@ -121,12 +58,6 @@ const Home = () => {
             </div>
         </section>) : (<h4>{status}</h4>);
 
-
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchData(page));
-    }, [dispatch, page]);
-
     const displayArt = artwork.length ? (
         <ul className={`${style.artList} pb-5`}>
         {
@@ -149,30 +80,7 @@ const Home = () => {
         }
         </ul>
     ) : (<section>{pageStatus}</section>);
-
-    // const list = meat.length ? (
-    //     <ul className={style.artList}>
-    //     {
-    //         meat.map((art, index) => (
-    //             <li key={art.id} className={`item${index}`} onClick={()=>navigate('/details', {
-    //                 state: {
-    //                     object: art,
-    //                 },
-    //             })}>
-    //             <img src={art[0].image} alt="" />
-    //             <div>
-    //             <i className="bi bi-arrow-right-circle-fill text-dark" style={{ fontSize: 20 }}></i>
-    //             <menu>
-    //             <h5 className="text-dark text-uppercase">{art[0].place_of_origin}</h5>
-    //             <h6 className="text-dark">{art.length} Artwork</h6>
-    //             </menu>
-    //             </div>
-    //             </li>
-    //         ))
-    //     }
-    //     </ul>
-    // ) : (<section>{pageStatus}</section>);
-
+    
     return (
         <>
         <Container className='mt-4 mb-4'>
@@ -189,7 +97,7 @@ const Home = () => {
         <div className='h6'>
         Arts by country
         </div>
-        <div  id={style.container}>
+        <div className="d-flex justify-content-center">
         {displayArt}
         {prev_page}
         {next_page}
